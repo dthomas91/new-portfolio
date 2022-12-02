5e91f9e23c22fbf2bd2d1e32697b81e24a599ede
dthomas91 <davidthomas8891@gmail.com>
2022-12-01 16:38:58 -0800

    updated icons

--- davids-portfolio/src/components/Header.js
+++ davids-portfolio/src/components/Header.js
@@ -5,14 +5,35 @@ const Header = () => {
     <>
       <div className="header">
         <div className="header_icons">
-          <i className="fa-brands fa-linkedin"></i>
+          <a
+            href="https://www.linkedin.com/in/david-thomas-856410166/"
+            target="_blank"
+          >
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
           <i className="fa-brands fa-github"></i>
         </div>
         <div className="header_links_wrapper">
-          <p><a className="header_links" href="./">Home</a></p>
-          <p><a className="header_links" href="./About">About</a></p>
-          <p><a className="header_links" href="./Projects">Projects</a></p>
-          <p><a className="header_links" href="./Contact">Contact</a></p>
+          <p>
+            <a className="header_links" href="./">
+              Home
+            </a>
+          </p>
+          <p>
+            <a className="header_links" href="./About">
+              About
+            </a>
+          </p>
+          <p>
+            <a className="header_links" href="./Projects">
+              Projects
+            </a>
+          </p>
+          <p>
+            <a className="header_links" href="./Contact">
+              Contact
+            </a>
+          </p>
         </div>
       </div>
     </>
