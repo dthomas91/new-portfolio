3afa567dae8005a84ade1691e0dc069e46026742
dthomas91 <davidthomas8891@gmail.com>
2022-12-14 08:27:07 -0800

    added typewritter

--- davids-portfolio/src/pages/Home.js
+++ davids-portfolio/src/pages/Home.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Typewriter from 'typewriter-effect';
 
 const Home = () => {
   return (
     <>
-     <h1>Home page</h1>
+      <h1>Home page</h1>
+      <div className="greeting">
+        <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+          .changeDelay(70)
+          .typeString("Welcome to my page!")
+          .pauseFor(1000)
+          .start()
+        }}
+        />
+      </div>
     </>
   );
 };
 
-export default Home
+export default Home;
